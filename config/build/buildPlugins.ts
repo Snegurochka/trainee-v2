b73e4e6178a924bf6bce3bf77ec74e5d1099b6b0
anastasiya <snegurka@web-esse.ru>
2024-08-25 09:09:48 +0500

    feat: prepared to publication

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -2,23 +2,27 @@ import webpack from "webpack";
 import HTMLWebpackPlugin from "html-webpack-plugin";
 import Dotenv from "dotenv-webpack";
 import { BuildOptions } from "./buildWebpackTypes";
+import CopyPlugin from "copy-webpack-plugin";
 
 export function buildPlugins({
   paths,
   isDev,
 }: BuildOptions): webpack.WebpackPluginInstance[] {
-
   return [
     new HTMLWebpackPlugin({
       template: paths.html,
     }),
     new webpack.ProgressPlugin(),
     new Dotenv({
-      path: './.env.local',
+      path: "./.env.local",
     }),
     new webpack.DefinePlugin({
-      __IS_DEV__: JSON.stringify(isDev)
+      __IS_DEV__: JSON.stringify(isDev),
     }),
-
+    new CopyPlugin({
+      patterns: [
+        { from: paths.locales, to: paths.buildLocales },
+      ],
+    }),
   ];
 }
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,25 +1,27 @@
-import webpack from 'webpack';
+import webpack from "webpack";
 import path from "path";
-import {buildWebpackConfig} from "./config/build/buildWebpackConfig";
-import { BuildPaths, BuildEnv } from './config/build/buildWebpackTypes';
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildPaths, BuildEnv } from "./config/build/buildWebpackTypes";
 
 export default (env: BuildEnv) => {
-    const paths: BuildPaths = {
-        entry: path.resolve(__dirname, 'src', 'index.tsx'),
-        build: path.resolve(__dirname, 'build'),
-        html: path.resolve(__dirname, 'public', 'index.html'),
-    }
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, "src", "index.tsx"),
+    build: path.resolve(__dirname, "build"),
+    html: path.resolve(__dirname, "public", "index.html"),
+    locales: path.resolve(__dirname, "public", "locales"),
+    buildLocales: path.resolve(__dirname, "build", "locales"),
+  };
 
-    const mode = env.mode || 'development';
-    const isDev = mode === 'development';
-    const PORT = env.port || 3000;
+  const mode = env.mode || "development";
+  const isDev = mode === "development";
+  const PORT = env.port || 3000;
 
-    const config: webpack.Configuration = buildWebpackConfig({
-        mode,
-        paths,
-        isDev,
-        port: PORT,
-    })
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
 
-    return config;
+  return config;
 };
