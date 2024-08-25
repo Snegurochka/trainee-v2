5a9d1e2437e3c86b7402a1f745779f50b88c25ef
anastasiya <snegurka@web-esse.ru>
2024-08-25 15:38:09 +0500

    fix: netlify

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./services/route/route";
 
 function App() {
+  console.log('start 2', process.env.REACT_APP_FIREBASE_API_KEY);
   return (
       <RouterProvider router={router} />
   );
